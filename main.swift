907d1ebb502cd863a6f8c953f6fbbf642f3873a1
JA Proger <134533485+ja-proger@users.noreply.github.com>
2025-06-14 02:39:02 +0300

    Update main.swift
    Added some structs

--- main.swift
+++ main.swift
@@ -1,1 +1,21 @@
 // main.swift 
+
+
+/// 
+struct StLib { }
+
+/// Probablity Theory Event
+extension StLib {
+  struct StEvent {
+  }
+}
+
+/// classic term of probability
+extension StLib {
+  func probability() -> Double {
+
+  }
+  func probability() -> String {
+    
+  }
+}
