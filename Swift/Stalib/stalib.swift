dead11730483060a7cbcb6c0bc08e56854f89ecc
JA Proger <134533485+ja-proger@users.noreply.github.com>
2025-06-14 14:23:59 +0300

    edited added basic structures

---
+++ Swift/Stalib/stalib.swift
@@ -0,0 +1,26 @@
+
+
+struct StaLib {}
+
+extension StaLib {
+    struct StaEvent {}
+}
+
+extension StaLib {
+    struct StaProb {
+        var prob: Double
+        var prob: String
+    }
+}
+
+extension StaLib {
+    func probability() -> String{
+
+    }
+
+    func probability() -> Double{
+
+    }
+}
+
+
